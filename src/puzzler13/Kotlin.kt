3b525d499e1cd9a3baf0f8725c6807f0088bc3fc
Anton Keks <anton@azib.net>
2016-11-10 21:10:38 +0200

    add System.getProperty() example

--- src/puzzler13/Kotlin.kt
+++ src/puzzler13/Kotlin.kt
@@ -3,3 +3,14 @@ package puzzler13
 class Kotlin {
   fun hello(name: String) = print("Hello $name")
 }
+
+fun main(args: Array<String>) {
+  val prop = System.getProperty("key")
+  Kotlin().hello(prop)
+}
+
+// What will it print?
+// a) Hello
+// b) Hello null
+// c) NullPointerException
+// d) IllegalStateException

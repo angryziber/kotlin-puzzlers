7fe89205a2d5a30c1bcb0428ab5eae84974b5b75
Anton Keks <anton@azib.net>
2016-11-10 20:44:38 +0200

    add named params puzzler

---
+++ src/puzzler15/NamedParams.kt
@@ -0,0 +1,22 @@
+package puzzler15
+
+open class C {
+  open fun sum(x: Int = 1, y: Int = 2): Int = x + y
+}
+
+class D : C() {
+  override fun sum(y: Int, x: Int): Int = super.sum(x, y)
+}
+
+fun main(args: Array<String>) {
+  val d: D = D()
+  val c: C = d
+  print(c.sum(x = 0))
+  print(d.sum(x = 0))
+}
+
+// What will it print?
+// a) 22
+// b) 11
+// c) 21
+// d) Will not compile

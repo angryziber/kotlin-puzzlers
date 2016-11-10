d131fcda355d4d08671a2dee1f5743b0b51b94f5
Anton Keks <anton@azib.net>
2016-11-10 17:40:54 +0200

    function declaration puzzler

---
+++ src/puzzler5/Functions.kt
@@ -0,0 +1,18 @@
+package puzzler5
+
+fun main(args: Array<String>) {
+  main2()
+  main3()
+}
+
+fun main2() = print("Hello")
+
+fun main3() = {
+  print("Hello")
+}
+
+// What will it print?
+// a) Hello
+// b) HelloHello
+// c) Nothing
+// d) Code does not compile

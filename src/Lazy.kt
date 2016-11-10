389ab1cd6d42abb5f4f98bef75e6f8fc2a35170f
Anton Keks <anton@azib.net>
2016-11-10 20:10:10 +0200

    lazy puzzler

---
+++ src/Lazy.kt
@@ -0,0 +1,24 @@
+class Lazy {
+  var x = 0
+  val y by lazy { 1/x }
+
+  fun hello() {
+    try {
+      print(y)
+    }
+    catch (e: Exception) {
+      x = 1
+      print(y)
+    }
+  }
+}
+
+fun main(args: Array<String>) {
+  Lazy().hello()
+}
+
+// What will it print?
+// a) 0
+// b) 1
+// c) NaN
+// d) ArithmeticException

dee44fdf3ba370db5f0356e3255a98870e3b36e1
Anton Keks <anton@azib.net>
2016-11-10 19:38:54 +0200

    add getter puzzler

---
+++ src/puzzler11/GetMeJohn.kt
@@ -0,0 +1,16 @@
+package puzzler11
+
+class Person(name: String) {
+  var name = name
+    get() = if (name == "John") "Jaan" else name
+}
+
+fun main(args: Array<String>) {
+  println(Person("John").name)
+}
+
+// What will it print?
+// a) John
+// b) Jaan
+// c) Will not compile
+// d) None of the above

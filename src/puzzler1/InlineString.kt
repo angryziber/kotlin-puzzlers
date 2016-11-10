d7602b351f93f99ac9a7eb3f0274758bc50035f6
Anton Keks <anton@azib.net>
2016-11-10 17:17:18 +0200

    first simple puzzler

---
+++ src/puzzler1/InlineString.kt
@@ -0,0 +1,12 @@
+package puzzler1
+
+fun main(args: Array<String>) {
+  val num = 123
+  println("The number is $num+1")
+}
+
+// What will it print?
+// a) The number is $num+1
+// b) The number is 124
+// c) The number is 123+1
+// d) The number is +1

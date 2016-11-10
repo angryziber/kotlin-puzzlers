1e2a7080178e198b300d1c824085cc9b8a32e25d
Anton Keks <anton@azib.net>
2016-11-10 17:57:51 +0200

    add MutableList

--- src/puzzler6/AListOrNotAList.kt
+++ src/puzzler6/AListOrNotAList.kt
@@ -2,13 +2,13 @@ package puzzler6
 
 fun main(args: Array<String>) {
   val x = listOf(1, 2, 3)
-  println(x is kotlin.collections.List)
   println(x is kotlin.collections.List<Int>)
+  println(x is kotlin.collections.MutableList)
   println(x is java.util.List<*>)
 }
 
 // What will it print?
 // a) true; true; true
 // b) true; true; false
-// c) false; true; false
-// d) false; true; true
+// c) true; false; false
+// d) true; false; true

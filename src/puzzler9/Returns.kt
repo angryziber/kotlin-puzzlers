9b9cabd981040cae47e6ecafccfaf0cebc834632
Anton Keks <anton@azib.net>
2016-11-13 15:33:22 +0200

    simplify puzzler

--- src/puzzler9/Returns.kt
+++ src/puzzler9/Returns.kt
@@ -1,18 +1,15 @@
 package puzzler9
 
-fun filterMe(arg: Int): Boolean {
-  val list = listOf(1, 2, 3).filter {
-    return it >= arg
-  }
-  return list.isNotEmpty()
-}
-
 fun main(args: Array<String>) {
-  print(filterMe(2))
+  listOf(1, 2, 3).forEach {
+    if (it > 2) return
+    print(it)
+  }
+  print("done")
 }
 
 // What will it print?
-// a) true
-// b) false
-// c) Will stuck in an infinite loop
-// d) Code will not compile
+// a) 123done
+// b) 12done
+// c) 12
+// d) Will stuck in an infinite loop

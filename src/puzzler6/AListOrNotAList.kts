61712136136c65fb43e0f46237106c8bf94b096c
Anton Keks <anton@azib.net>
2016-11-15 22:25:57 +0300

    change variant

--- src/puzzler6/AListOrNotAList.kts
+++ src/puzzler6/AListOrNotAList.kts
@@ -6,7 +6,7 @@ println(x is kotlin.collections.MutableList)
 println(x is java.util.List<*>)
 
 // What will it print?
-// a) true; true; false
+// a) true; false; true
 // b) true; false; false
 // c) false; false; true
 // d) true; true; true

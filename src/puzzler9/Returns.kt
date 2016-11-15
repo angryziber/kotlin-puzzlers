01fc5266ea25d0ee42f5c2b11bf3b586b87619a3
Anton Keks <anton@azib.net>
2016-11-15 22:05:07 +0300

    kotlin 1.0.5 crashes when executing this code as script...

---
+++ src/puzzler9/Returns.kt
@@ -0,0 +1,15 @@
+package puzzler9
+
+fun main(args: Array<String>) {
+  listOf(1, 2, 3).forEach {
+    if (it > 2) return
+    print(it)
+  }
+  print("ok")
+}
+
+// What will it print?
+// a) 123ok
+// b) 12ok
+// c) 12
+// d) Infinite loop

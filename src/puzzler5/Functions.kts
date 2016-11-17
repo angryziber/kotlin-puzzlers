ec74e867a7abc635bc3e7506a3116ee67d1c18fd
Anton Keks <anton@azib.net>
2016-11-17 18:26:57 +0200

    kts file should not contain a main method

--- src/puzzler5/Functions.kts
+++ src/puzzler5/Functions.kts
@@ -1,15 +1,13 @@
 package puzzler5
 
-fun main(args: Array<String>) {
-  main2()
-  main3()
+fun main1() = print("Hello")
+
+fun main2() = {
+  print("Hello3")
 }
 
-fun main2() = print("Hello")
-
-fun main3() = {
-  print("Hello")
-}
+main1()
+main2()
 
 // What will it print?
 // a) Hello

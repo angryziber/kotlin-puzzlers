cc0de167b81f646ac1610a03a24bb40af653d495
Anton Keks <anton@azib.net>
2017-02-06 19:48:36 +0200

    issue #3 - add unit puzzler

---
+++ src/puzzler5/WhatAmI.kts
@@ -0,0 +1,10 @@
+package puzzler5
+
+val whatAmI = {}()
+println(whatAmI)
+
+// What will it print?
+// a) kotlin.jvm.functions.Function0
+// b) () -> kotlin.Unit
+// c) kotlin.Unit
+// d) will not compile

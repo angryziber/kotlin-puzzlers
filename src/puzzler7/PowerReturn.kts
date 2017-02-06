daa59107eae776ab0422a62d5461d8de2dce4d1e
Anton Keks <anton@azib.net>
2017-02-06 20:08:16 +0200

    issue #3 - add power throw and power return

---
+++ src/puzzler7/PowerReturn.kts
@@ -0,0 +1,13 @@
+package puzzler7
+
+fun hello(): String {
+  return return return "Hello"
+}
+
+print(hello())
+
+// What will it print?
+// a) Hello
+// b) Hello Hello Hello
+// c) nothing
+// d) will not compile
---
+++ src/puzzler7/PowerThrow.kts
@@ -0,0 +1,10 @@
+package puzzler7
+
+throw throw throw Exception()
+// return return 123
+
+// What will it print?
+// a) java.lang.Exception
+// b) 3x java.lang.Exception
+// c) nothing
+// d) will not compile

adb518333505d8d5c52d995c21749945f4d7fd5c
voddan <dgvodopyan@gmail.com>
2017-04-11 19:06:36 +0300

    Added inclusive-ranges

---
+++ src/collections/inclusiveRanges/inclusive-ranges.kts
@@ -0,0 +1,11 @@
+package collections.inclusiveRanges
+
+val range = 0..9 step 3
+
+println(range.joinToString())
+
+// What will it print?
+// a) 3, 6
+// b) 0, 3, 6
+// c) 0, 3, 6, 9
+// d) Will not compile

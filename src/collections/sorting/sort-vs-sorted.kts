781b3efbd22ef52b4a51175e1578afa310a0ac93
voddan <dgvodopyan@gmail.com>
2017-04-11 23:23:35 +0300

    Added sort-vs-sorted

---
+++ src/collections/sorting/sort-vs-sorted.kts
@@ -0,0 +1,13 @@
+package collections.sorting
+
+val list = arrayListOf(1, 5, 3, 2, 4)
+
+val sortedList = list.sort()
+
+print(sortedList)
+
+// What will it print?
+// a) [1, 5, 3, 2, 4]
+// b) [1, 2, 3, 4, 5]
+// c) kotlin.Unit
+// d) Will not compile

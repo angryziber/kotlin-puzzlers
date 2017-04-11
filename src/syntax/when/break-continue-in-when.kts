ba0e83de3d6fa8a62285a02fd5defe15a77678f1
voddan <dgvodopyan@gmail.com>
2017-04-12 00:20:54 +0300

    minor cosmetics

--- src/syntax/when/break-continue-in-when.kts
+++ src/syntax/when/break-continue-in-when.kts
@@ -1,23 +1,21 @@
 package syntax.`when`
 
-fun printToPunctuation(str: String) {
+fun printFormatted(str: String) {
     for(ch in str) {
-
         when(ch) {
             'C' -> continue
             'R' -> return
             'B' -> break
         }
-
         print(ch)
     }
 
     print("; ")
 }
 
-printToPunctuation("1C2")
-printToPunctuation("3B4")
-printToPunctuation("5R6")
+printFormatted("1C2")
+printFormatted("3B4")
+printFormatted("5R6")
 
 
 // What does it print?

3523d227d75e37bd279b2811aaa6c31d7325febd
Anton Keks <anton@azib.net>
2017-04-12 22:46:21 +0300

    move submitted puzzler to syntax

---
+++ src/syntax/manyReturns/ManyReturns.kts
@@ -0,0 +1,16 @@
+package syntax.manyReturns
+
+fun isItSupposedToBeFun(condition: Boolean): String {
+    return if (condition)
+        return "yep. It's fun"
+    else
+        return "nope. Nothing special"
+}
+
+println(isItSupposedToBeFun(true))
+
+// What will it print?
+// a) yep. It's fun
+// b) yep. It's fun and warning: unreachable code
+// c) yep. It's funnope. Nothing spacial
+// d) Compilation error: unreachable code

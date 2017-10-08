8e4c3802b689ba8be6061b0cfef9ffc166afa9bd
Anton Keks <anton@azib.net>
2017-10-08 21:00:13 +0300

    rename file and use cities relevant for Kotlinconf

---
+++ src/delegates/map/PopulationToMars.kts
@@ -0,0 +1,22 @@
+package delegates.map
+
+class Population(var cities: Map<String, Int>) {
+    val `san francisco` by cities
+    val `st petersburg` by cities
+    val `tallinn` by cities
+}
+
+val population = Population(mapOf("san francisco" to 864_816, "st petersburg" to 5_281_579 , "tallinn" to 413_782))
+
+/* Many years have passed, now all humans live on Mars */
+population.cities = emptyMap()
+
+with(population) {
+    println("$`san francisco`; $`st petersburg`; $`tallinn`")
+}
+
+// What will it print?
+// a) 0; 0; 0
+// b) 864816; 5281579; 413782
+// c) NullPointerException
+// d) NoSuchElementException

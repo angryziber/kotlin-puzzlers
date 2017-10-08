c8b0fc4db3be553fb129fe21719ab68d8b0b993d
Anton Keks <anton@azib.net>
2017-10-08 22:18:08 +0300

    rename to very smart cast (make it a kt so we can show bytecode)

---
+++ src/types/verySmartCast/VerySmartCast.kt
@@ -0,0 +1,13 @@
+package types.verySmartCast
+
+fun main(args: Array<String>) {
+  val x: Any = "123"
+  val z = if (x is Int && x is String) x.length + x.inc() else 123
+  println(z)
+}
+
+// What will it print?
+// a) 123
+// b) 127
+// c) ClassCastException
+// d) will not compile

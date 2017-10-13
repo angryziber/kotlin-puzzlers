76d04a5a8b84aac78cc30071a12ec7ab4972ac74
Anton Keks <anton@azib.net>
2017-10-14 00:08:30 +0300

    add data/Copy puzzler

---
+++ src/data/Copy.kts
@@ -0,0 +1,17 @@
+package data
+
+data class Container(val list: MutableList<String>)
+
+val list = mutableListOf("one", "two")
+val c1 = Container(list)
+val c2 = c1.copy()
+list += "oops"
+println(c2.list.joinToString())
+
+// What will it print?
+// a) one, two
+// b) one, two, oops
+// c) UnsupportedOperationException
+// c) will not compile
+
+// Inspired by Adam Arold @addamsson
---
+++ src/data/Rationale.md
@@ -0,0 +1,5 @@
+Correct answer: **b) one, two, oops**
+
+* `copy()` method of a data class does a shallow clone,
+   i.e. copying only references to the fields
+*  Make data classes immutable to avoid this problem 

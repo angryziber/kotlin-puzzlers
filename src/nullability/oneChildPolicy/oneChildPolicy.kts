39ae80434b81c080e4fffb42cef452d26fda9b27
Anton Keks <anton@azib.net>
2017-10-13 22:17:22 +0300

    #23 add oneChildPolicy puzzler, thanks @dkandalov for suggestion!

---
+++ src/nullability/oneChildPolicy/oneChildPolicy.kts
@@ -0,0 +1,22 @@
+package nullability.oneChildPolicy
+
+open class Node(val name: String) {
+  fun lookup() = println(name)
+}
+
+class Parent : Node("parent") {
+  fun child(name: String): Node? = Node(name)
+
+  val child1 = child("child1")?.apply { lookup() }
+  val child2 = child("child2").apply { lookup() }
+}
+
+Parent()
+
+// What will it print?
+// a) child1; child2
+// b) child1; parent
+// c) parent; child2
+// d) none of the above
+
+// Inspired by Roman Dawydkin @beholder

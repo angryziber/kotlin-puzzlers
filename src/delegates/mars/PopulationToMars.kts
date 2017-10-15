4a58542297b4fd732c608780adf537ef491a5536
Anton Keks <anton@azib.net>
2017-10-15 22:29:42 +0300

    change st petersburg to kotlin island :-)

--- src/delegates/mars/PopulationToMars.kts
+++ src/delegates/mars/PopulationToMars.kts
@@ -2,25 +2,25 @@ package delegates.mars
 // by Daniil Vodopian @voddan
 
 class Population(var cities: Map<String, Int>) {
-    val `san francisco` by cities
-    val `st petersburg` by cities
-    val `tallinn` by cities
+  val `san francisco` by cities
+  val `tallinn` by cities
+  val `kotlin` by cities
 }
 
 val population = Population(mapOf(
     "san francisco" to 864_816,
-    "st petersburg" to 5_281_579 ,
-    "tallinn" to 413_782))
+    "tallinn" to 413_782,
+    "kotlin" to 43_005))
 
-// Many years have passed, now all humans live on Mars
+// many years have passed, now all humans live on Mars
 population.cities = emptyMap()
 
 with(population) {
-    println("$`san francisco`; $`st petersburg`; $`tallinn`")
+  println("$`san francisco`; $tallinn; $kotlin")
 }
 
 // What will it print?
 // a) 0; 0; 0
-// b) 864816; 5281579; 413782
+// b) 864816; 413782; 43005
 // c) NullPointerException
 // d) NoSuchElementException
--- src/delegates/mars/Rationale.md
+++ src/delegates/mars/Rationale.md
@@ -1,3 +1,3 @@
-Correct answer: **b) 864816; 5281579; 413782**
+Correct answer: **b) 864816; 413782; 43005**
 
 When delegating, the delegate object is saved into a final field and cannot be changed

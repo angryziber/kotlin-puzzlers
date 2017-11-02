df498105053a1473cf8112af6a3ab36575eb820a
Anton Keks <anton@azib.net>
2017-11-02 18:06:48 +0200

    disable warnings

--- run-kts.sh
+++ run-kts.sh
@@ -16,7 +16,7 @@ DIR=`dirname $FILE`
 
 echo "Drum roll..." >&2
 screen -d -m mpg123 -k 50 drumroll.mp3
-java -cp $IDEA/plugins/Kotlin/kotlinc/lib/kotlin-compiler.jar:$IDEA/plugins/Kotlin/kotlinc/lib/kotlin-reflect.jar:$IDEA/plugins/Kotlin/kotlinc/lib/kotlin-runtime.jar:$IDEA/plugins/Kotlin/kotlinc/lib/kotlin-script-runtime.jar org.jetbrains.kotlin.cli.jvm.K2JVMCompiler -script $FILE
+java -cp $IDEA/plugins/Kotlin/kotlinc/lib/kotlin-compiler.jar:$IDEA/plugins/Kotlin/kotlinc/lib/kotlin-reflect.jar:$IDEA/plugins/Kotlin/kotlinc/lib/kotlin-runtime.jar:$IDEA/plugins/Kotlin/kotlinc/lib/kotlin-script-runtime.jar org.jetbrains.kotlin.cli.jvm.K2JVMCompiler -nowarn -script $FILE
 
 sleep 1
 mpv --quiet --no-osc --ontop --no-border --autofit=50%x50% --geometry=100%:0% --loop $DIR/giphy.* 2>/dev/null >/dev/null

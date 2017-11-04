ae378893c6edacbbba02269aa82dc2edad36c5ee
hkurokawa <hirosh.kurokawa@gmail.com>
2017-11-04 21:38:03 +0900

    Add a puzzle of reference equality.

---
+++ src/types/referenceEquality/referenceEquality.kts
@@ -0,0 +1,8 @@
+println(127 as Int? === 127 as Int?)
+println(128 as Int? === 128 as Int?)
+
+// What will it print?
+// a) true; true
+// b) false; false
+// c) true; false
+// d) none of the above

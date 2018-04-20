faceca730a5df6035eee94c0bdd15291a033caa0
Marat Kh. Akhin <Marat.Akhin@gmail.com>
2018-04-20 11:58:44 +0700

    Add NothingYet and CompareMuch puzzlers

---
+++ src/bugs/compareMuch/CompareMuch.kts
@@ -0,0 +1,19 @@
+package bugs.compareMuch
+
+import java.util.Random
+
+val rnd = Random()
+
+val a = rnd.nextInt()
+val b = rnd.nextInt()
+val c = rnd.nextInt()
+
+fun whoWon(vararg conds: Boolean) = conds.all { true == it }
+
+println(whoWon(a < b, b > c))
+
+// What will it print?
+// a) true
+// b) false
+// c) IllegalArgumentException
+// d) will not compile

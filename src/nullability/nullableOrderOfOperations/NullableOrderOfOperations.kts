ee6b014a3787428280fbf8414cd53887a9efc03e
Thomas Nield <thomasnield@live.com>
2018-06-04 18:59:08 -0500

    add nullable order of operations

---
+++ src/nullability/nullableOrderOfOperations/NullableOrderOfOperations.kts
@@ -0,0 +1,16 @@
+package nullability.nullableOrderOfOperationsas
+// by Thomas Nield @thomasnield
+
+val x: Int? = 2
+val y: Int = 3
+
+val sum = x?:0 + y
+
+println(sum)
+
+
+// What will it print?
+// a) 3
+// b) 5
+// c) 2
+// d) 0

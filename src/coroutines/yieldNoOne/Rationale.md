012e3709ee9c036ee8f0e27e053bc4e1950393e2
Anton Keks <anton@azib.net>
2018-09-17 23:51:43 +0300

    add first coroutine puzzler

---
+++ src/coroutines/yieldNoOne/Rationale.md
@@ -0,0 +1,4 @@
+Correct answer: **d) none of the above (kotlin.sequences.TakeSequence@34e40e50)**
+
+* buildSequence/yield actually would produce **b) [0, 1, 2]**
+* but kotlin.sequence doesn't have a nice toString() implementation
---
+++ src/coroutines/yieldNoOne/YieldNoOne.kts
@@ -0,0 +1,14 @@
+package coroutines.yieldNoOne
+
+val x = buildSequence {
+  var n = 0
+  while (true) yield(n++)
+}
+
+println(x.take(3))
+
+// What will it print?
+// a) [1, 2, 3]
+// b) [0, 1, 2]
+// c) [0, 0, 0]
+// d) none of the above

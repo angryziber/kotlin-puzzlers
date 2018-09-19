6dc8c96fd2880851bd679a8e0b22482b080b3cb3
Anton Keks <anton@azib.net>
2018-09-19 18:02:21 +0300

    covert to kts

--- src/interop/platformNulls/PlatformNulls.kts
+++ src/interop/platformNulls/PlatformNulls.kts
@@ -4,13 +4,11 @@ class Kotlin {
   fun hello(name: String) = print("Hello $name")
 }
 
-fun main(args: Array<String>) {
-  val prop = System.getProperty("key")
-  Kotlin().hello(prop)
-}
+val prop = System.getProperty("key")
+Kotlin().hello(prop)
 
 // What will it print?
 // a) Hello
 // b) Hello null
-// c) Will not compile
-// d) None of the above
+// c) will not compile
+// d) none of the above

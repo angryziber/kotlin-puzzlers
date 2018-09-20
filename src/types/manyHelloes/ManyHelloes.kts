f685e03b8f09c6411f105f7f0697cf9da994b659
Anton Keks <anton@azib.net>
2018-09-20 12:18:35 +0300

    add many helloes puzzler

---
+++ src/types/manyHelloes/ManyHelloes.kts
@@ -0,0 +1,13 @@
+package types.manyHelloes
+
+fun hello(): Boolean {
+  println(print("Hello") == print("World") == return false)
+}
+
+hello()
+
+// What will it print?
+// a) HelloWorld
+// b) HelloWorldfalse
+// c) HelloWorldtrue
+// d) will not compile
---
+++ src/types/manyHelloes/Rationale.md
@@ -0,0 +1,5 @@
+Correct answer: **a) HelloWorld**
+
+* 'void' methods return Unit
+* Unit is a singleton, so equality holds
+* return expression's type is Nothing, but return does it's thing and outer println is not executed

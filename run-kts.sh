779459f832e22b8a43f7ec4c6af148db66549578
Anton Keks <anton@azib.net>
2018-09-20 18:23:11 +0300

    run kt files as well - inline classes don't work in kts files

--- run-kts.sh
+++ run-kts.sh
@@ -16,8 +16,13 @@ DIR=`dirname $FILE`
 
 echo "Drum roll..." >&2
 screen -d -m mpg123 -k 50 drumroll.mp3
-java -cp $KOTLIN_LIB'/*' org.jetbrains.kotlin.cli.jvm.K2JVMCompiler \
-     -nowarn -Xuse-experimental=kotlin.ExperimentalUnsignedTypes -script $FILE
+
+if [[ $FILE == *.kts ]]; then
+    java -cp $KOTLIN_LIB'/*' org.jetbrains.kotlin.cli.jvm.K2JVMCompiler \
+         -nowarn -Xuse-experimental=kotlin.ExperimentalUnsignedTypes -script $FILE
+else
+    java -cp $KOTLIN_LIB'/*':out/production/kotlin-puzzlers $CLASS
+fi
 
 sleep 1
 mpv --quiet --no-osc --ontop --no-border --autofit=50%x50% --geometry=100%:0% --loop $DIR/giphy.* 2>/dev/null >/dev/null

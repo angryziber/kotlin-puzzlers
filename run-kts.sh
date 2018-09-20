b219d7a4ec3e99209bdbd3e964b2b15b36c397a9
Anton Keks <anton@azib.net>
2018-09-20 17:08:29 +0300

    use kotlin 1.3 in the runner (from the latest plugin), as well as enable experimental features

--- run-kts.sh
+++ run-kts.sh
@@ -6,7 +6,7 @@
 FILE=$1
 CLASS=$2
 
-IDEA=~/apps/idea
+KOTLIN_LIB=~/.IntelliJIdea2018.2/config/plugins/Kotlin/kotlinc/lib
 
 if [ -z "$2" ]; then
   echo "2 params required"
@@ -16,7 +16,8 @@ DIR=`dirname $FILE`
 
 echo "Drum roll..." >&2
 screen -d -m mpg123 -k 50 drumroll.mp3
-java -cp $IDEA/plugins/Kotlin/kotlinc/lib/kotlin-compiler.jar:$IDEA/plugins/Kotlin/kotlinc/lib/kotlin-reflect.jar:$IDEA/plugins/Kotlin/kotlinc/lib/kotlin-runtime.jar:$IDEA/plugins/Kotlin/kotlinc/lib/kotlin-script-runtime.jar org.jetbrains.kotlin.cli.jvm.K2JVMCompiler -nowarn -script $FILE
+java -cp $KOTLIN_LIB'/*' org.jetbrains.kotlin.cli.jvm.K2JVMCompiler \
+     -nowarn -Xuse-experimental=kotlin.ExperimentalUnsignedTypes -script $FILE
 
 sleep 1
 mpv --quiet --no-osc --ontop --no-border --autofit=50%x50% --geometry=100%:0% --loop $DIR/giphy.* 2>/dev/null >/dev/null

2d4b818cc6ee35702d2826c3b160751a2195be67
Anton Keks <anton@azib.net>
2018-09-29 15:50:11 +0300

    add another puzzler with line breaks

---
+++ src/syntax/gameOfLife/GameOfLife.kts
@@ -0,0 +1,15 @@
+val cells = arrayOf(intArrayOf(1, 0, 1),
+                    intArrayOf(0, 1, 1),
+                    intArrayOf(1, 1, 1))
+
+var neighbors = cells[0][0] + cells[0][1] + cells[0][2]
+              + cells[1][0]               + cells[1][2]
+              + cells[2][0] + cells[2][1] + cells[2][2]
+
+print(neighbors)
+
+// What will it print?
+// a) 6
+// b) 3
+// c) 2
+// d) will not compile
---
+++ src/syntax/gameOfLife/Rationale.md
@@ -0,0 +1,3 @@
+Correct answer: **c) 2**
+
+* Same as `breakingLines`, lines starting with `+` are separate expressions with unary plus in front

2b31f2ca4ba1f3da6d96dc61cecfc6c454753ed7
Anton Keks <anton@azib.net>
2018-09-30 21:27:03 +0300

    add a funny array initializer function

--- src/collections/sorting/Sorting.kts
+++ src/collections/sorting/Sorting.kts
@@ -1,13 +1,13 @@
 package collections.sorting
 
-val list = arrayListOf(1, 5, 3, 2, 4)
+fun <T> L(vararg i: T) = arrayListOf(*i)
 
+val list = L(1, 5, 3, 2, 4)
 val sortedList = list.sort()
-
 print(sortedList)
 
 // What will it print?
-// a) [1, 5, 3, 2, 4]
-// b) [1, 2, 3, 4, 5]
+// a) [1, 2, 3, 4, 5]
+// b) [1, 5, 3, 2, 4]
 // c) will not compile
 // d) none of the above

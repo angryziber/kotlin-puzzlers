4dbbc7f501d8689485a992ba4443ec6fe43be996
Anton Keks <anton@azib.net>
2018-10-04 11:19:33 +0200

    add -progressive when running the puzzlers - doesn't change anything for now

--- run-kts.sh
+++ run-kts.sh
@@ -19,10 +19,10 @@ screen -d -m mpg123 -k 50 drumroll.mp3
 
 if [[ $FILE == *.kts ]]; then
     java -cp $KOTLIN_LIB'/*' org.jetbrains.kotlin.cli.jvm.K2JVMCompiler \
-         -nowarn -Xuse-experimental=kotlin.ExperimentalUnsignedTypes -script $FILE
+         -nowarn -progressive -Xuse-experimental=kotlin.ExperimentalUnsignedTypes -script $FILE
 else
     java -cp $KOTLIN_LIB'/*':out/production/kotlin-puzzlers $CLASS
 fi
 
 sleep 1
-mpv --quiet --no-osc --ontop --no-border --autofit=50%x50% --geometry=100%:0% --loop $DIR/giphy.* 2>/dev/null >/dev/null
+#mpv --quiet --no-osc --ontop --no-border --autofit=50%x50% --geometry=100%:0% --loop $DIR/giphy.* 2>/dev/null >/dev/null

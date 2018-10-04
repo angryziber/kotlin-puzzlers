404c3f8b326b62a411633849f4bb7bf6e43ac687
Anton Keks <anton@azib.net>
2018-10-04 21:52:40 +0200

    reformat

--- src/types/sealedClass/sealedClass.kts
+++ src/types/sealedClass/sealedClass.kts
@@ -1,16 +1,15 @@
 package types.sealedClass
 
-sealed class Animal{
-    class Human : Animal()
-    class Dog : Animal()
-}
-fun test_sealed_class_exhaustiveness() {
-    val h = Animal.Human() as Animal
-    when (h) {
-        is Animal.Dog -> println("It is a Dog")
-    }
+sealed class Animal {
+  class Human : Animal()
+  class Dog : Animal()
 }
 
-//Will it compile?
-//a) No
-//b) Yes
+val h = Animal.Human() as Animal
+when (h) {
+  is Animal.Dog -> println("It is a Dog")
+}
+
+// Will it compile?
+// a) No
+// b) Yes

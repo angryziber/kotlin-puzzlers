8c2b9c5e7449d4e90358f0f9a0198a8964b5ffac
Alexey Hanin <alexey.hanin@applift.com>
2018-11-12 17:25:59 +0100

    Add example of implicit fallback to a default namespace function

---
+++ src/types/phantomInfixFunction/PhantomInfixFunction.kts
@@ -0,0 +1,26 @@
+package types.phantomInfixFunction
+
+class PersonBuilder {
+
+    infix fun String.to(value: Any) {
+        print(value)
+    }
+}
+
+fun person(block: PersonBuilder.() -> Unit) {
+    PersonBuilder().block()
+}
+
+class John
+
+person {
+    "firstname" to John::class.simpleName
+    "lastname" to "Doe"
+}
+
+// What will it print?
+// a) JohnDoe
+// b) John
+// c) Doe
+// d) will not compile
+// e) none of the above

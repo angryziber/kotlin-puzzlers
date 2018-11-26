fa3b5d92c4b35da3a186ba5fa2de8d27e2f8c2d6
bipokot <10675204+bipokot@users.noreply.github.com>
2018-11-27 01:11:49 +0300

    Added an orderOfInitialization puzzle

---
+++ src/types/orderOfInitialization/OrderOfInitialization.kts
@@ -0,0 +1,29 @@
+import Greeting.Companion.DEFAULT
+
+class Message(val text: String)
+
+sealed class Greeting(val message: Message) {
+
+    object UniverseGreeting : Greeting(Message("Universe"))
+    object WorldGreeting : Greeting(WORLD_MESSAGE)
+
+    companion object {
+        val DEFAULT = WorldGreeting
+        val WORLD_MESSAGE = Message("World")
+    }
+}
+
+fun printGreeting(greeting: Greeting?) {
+    with(greeting ?: DEFAULT) {
+        print("Hello${message.text}!")
+    }
+}
+
+printGreeting(null)
+printGreeting(Greeting.UniverseGreeting)
+
+// What will it print?
+// a) will not compile
+// b) HelloWorld!HelloUniverse!
+// c) NullPointerException
+// d) ExceptionInInitializerError
---
+++ src/types/orderOfInitialization/Rationale.md
@@ -0,0 +1,3 @@
+Correct answer: **c) NullPointerException**
+
+* When the `WorldGreeting` is constructed, the `WORLD_MESSAGE` has not been constructed yet and has a value of `null`.

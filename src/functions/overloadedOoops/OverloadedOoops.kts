3e8b1774626546e8e0928899ac25a3a4395297c7
Anton Keks <anton@azib.net>
2019-09-30 22:09:53 +0300

    simplify more as suggested by the original author in PR comments, also add a Rationale

--- src/functions/overloadedOoops/OverloadedOoops.kts
+++ src/functions/overloadedOoops/OverloadedOoops.kts
@@ -1,23 +1,17 @@
 package functions.overloadedOoops
 // by @ConstOrVar
 
-fun test(block: () -> Int = { 0 }): String {
-  return "${block()}"
-}
+fun test(block: () -> Int = { 0 }) = "${block()}"
 
-fun test2(block: () -> Int = { 0 }): () -> String {
-  return { test(block) }
-}
+val a = test { 2 }
+val b = test()
 
-{ println("Start") }()
-
-val a = test { 2 } == test2 { 2 }()
-val b = test() == test2()()
-
-{ println("Finish $a $b") }()
+//print("Debug")
+//{ 1 }
+println("$a $b")
 
 // What will it print?
-// a) true true
-// b) true false
-// c) true Unit
+// a) 2 0
+// b) 2 1
+// c) 2 Unit
 // d) Will not compile
---
+++ src/functions/overloadedOoops/Rationale.md
@@ -0,0 +1,5 @@
+Correct answer: **b) 2 1**
+
+* Lambda body can follow the function brackets `()`, even if on a different line
+* ... and there's a comment in between
+* Try uncommenting the `//print("Debug")` statement to make code not compile anymore

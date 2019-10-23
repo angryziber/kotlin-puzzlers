9557f945d93598935f8eee82bd0f7f822fc84681
Anton Keks <anton@azib.net>
2019-10-23 22:36:21 +0300

    #61 add suspendedNothing puzzler, thanks for an idea!

---
+++ src/coroutines/suspendNothing/suspendNothing.kts
@@ -0,0 +1,18 @@
+// after Grigorii Yurkov @rpuxa
+
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.intrinsics.*
+
+suspend fun absolute(): Nothing = suspendCoroutineUninterceptedOrReturn {}
+
+fun printNothing(nothing: Nothing) = print(nothing.toString())
+
+runBlocking {
+  printNothing(absolute())
+}
+
+// What will it print?
+// a) kotlin.Nothing
+// b) java.lang.Void
+// c) An exception
+// d) Will not compile

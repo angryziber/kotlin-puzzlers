e7dfb44ae770186848fdcb3b31af292dbb34d27e
Anton Keks <anton@azib.net>
2019-11-08 18:14:08 +0200

    #63 add bendingTheRules puzzler, thanks!

---
+++ src/contracts/bendingTheRules/BendingTheRules.kt
@@ -0,0 +1,26 @@
+package contracts.bendingTheRules
+// by Grigorii Yurkov @rpuxa
+
+import kotlin.contracts.*
+
+inline fun runLambda(block: () -> Unit) {
+  contract {
+    callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+  }
+}
+
+fun hello(): String {
+  var hello: String
+  runLambda { hello = "Kotlin" }
+  return hello
+}
+
+fun main() {
+  println(hello().plus(hello()))
+}
+
+// What will it print?
+// a) KotlinKotlin
+// b) NullPointerException
+// c) nullnull
+// d) Will not compile
---
+++ src/contracts/bendingTheRules/Rationale.md
@@ -0,0 +1,6 @@
+Correct answer: **c) null**
+
+* We trick the compiler using the EXACTLY_ONCE contract (meaning that the lambda is called exactly once), but we don't do this.
+* Kotlin compiler doesn't check if contract conforms for reality.
+* The compiler still initializes the variable to null before the `runLambda()` call, so `hello()` returns null
+* `hello().plus(hello())` doesn't result in NPE because it is compiled into `StringBuilder().append()`
